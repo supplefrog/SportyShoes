9335e9b52b072834e9c0b8d32bca3013ddce8a41
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-09-30 14:20:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sporty Shoes
+An online storefront for a sports shoe shop. Admins can login to manage and categorize products, browse and modify user data, and see purchase reports filtered by date and category.
---
+++ src/main/java/com/sportyshoes/config/UserData.java
@@ -0,0 +1,53 @@
+package com.sportyshoes.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserData implements CommandLineRunner {
+
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        /*UserEntity admin = new UserEntity("Riyaz J",
+                "j.riyazu@gmail.com",
+                "Male",
+                passwordEncoder.encode("admin"),
+                "KR Puram",
+                "Bengaluru",
+                "Karnataka",
+                "560006",
+                true,
+                true,
+                true,
+                true,
+                true,
+                UserRole.ROLE_ADMIN);
+
+        UserEntity user = new UserEntity("Khan",
+                "user@gmail.com",
+                "Male",
+                passwordEncoder.encode("user"),
+                "KR Puram",
+                "Bengaluru",
+                "Karnataka",
+                "560006",
+                true,
+                true,
+                true,
+                true,
+                true,
+                UserRole.ROLE_USER);
+
+        List<UserEntity> users = new ArrayList<>();
+        users.add(admin);
+        users.add(user);
+        userService.saveAll(users);*/
+    }
+}
